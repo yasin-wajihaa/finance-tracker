5a4338a2838be13b89a41801cb96d22014c75e2a
yasin-wajihaa <wajihaazafar@gmail.com>
2025-09-21 21:51:42 +0530

    added the transaction drawer

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 lucide.createIcons();
+
+const drawer = document.getElementById("transaction-adding-drawer")
+const addBtn = document.querySelector(".add-btn") 
+const form = document.getElementById("transaction-form")
+
+//  open drawer
+
+addBtn.addEventListener("click", () => {
+    drawer.classList.add("open");
+});
+
+// Handle form submission
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+    const desc = document.getElementById("desc").value;
+    const amount = parseFloat(document.getElementById("amount").value);
+    const type = document.getElementById("type").value;
+
+    // Example transaction object
+
+    const transaction = {desc, amount, type, date: new Date() };
+
+    // Push into transaction list andd update ui
+
+    drawer.classList.remove("open"); 
+    form.reset();
+});
